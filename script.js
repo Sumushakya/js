676658c178bcecc10294d99a06d0905659f0a410
sumima shakya <sumushakya25@gmail.com>
2024-04-16 08:51:06 +0545

    DOM

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log("hello");
-alert("hi");
+//let button = document.getElementById("myId");
+//console.dir(button);
+
+//let headings = document.getElementsByClassName("myClass");
+//console.dir(headings);
+//console.log(headings);
+
+//let par = document.getElementsByTagName("p");
+//console.dir(par);
+
+//let elements= document.querySelector("p"); //1st element
+//console.dir(elements);
+
+let elements= document.querySelectorAll(".myClass");//all elements
+console.dir(elements);
+
+
