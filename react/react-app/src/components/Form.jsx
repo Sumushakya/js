11e66df98f6f52745f073bb6cf60f4c3186edfd7
sumima shakya <sumushakya25@gmail.com>
2024-06-07 06:37:57 +0545

    Handling form submission

--- react/react-app/src/components/Form.jsx
+++ react/react-app/src/components/Form.jsx
@@ -1,7 +1,34 @@
+import {useState} from "react";
+
 export default function Form(){
-    return(<div>
+    const[name,setName]=useState({firstName:"", lastName:"",address:""});
+    // function handleChange(e){
+    //     setName(e.target.value);
+    //}
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(name)
+    }
+    return(
+    <div>
+        {name.firstName}-{name.lastName}-{name.address}
         <form>
-            <input type="text" value="Firstname" />
+            <input 
+            //  onChange= {function demo(e){return handleChange(e)}}
+            // onChange={(e)=>handleChange(e)} //onChange={(e) => setName(e.target.value)}
+             onChange={(e) => setName({...name,firstName:e.target.value})}
+             type="text" value={name.firstName} />
+             
+             <input 
+             onChange={(e) => setName({...name,lastName:e.target.value})}
+             type="text" value={name.lastName}/>
+
+            <input 
+             onChange={(e) => setName({...name,address:e.target.value})}
+             type="text" value={name.address}/>
+
+             <button onClick={(e => handleSubmit(e))}>Add</button>  
         </form>
-    </div>);
+    </div>
+    );
 }
