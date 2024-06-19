fcc6ce19cdb44073602168972608e4703436861a
sumima shakya <sumushakya25@gmail.com>
2024-06-19 10:01:10 +0545

    api,creating nav component,store data

--- react/foodapp/src/App.jsx
+++ react/foodapp/src/App.jsx
@@ -1,9 +1,13 @@
+import Nav from "./components/Nav";
 import Search from "./components/search";
+import "./App.css";
 
 function App() {
+  const [foodData, setfoodData] = useState([]);
   return (
     <div className="App">
-      <Search />
+      <Nav />
+      <Search foodData={foodData} setFoodData={setfoodData} />
     </div>
   );
 }
--- react/foodapp/src/components/Search.jsx
+++ react/foodapp/src/components/Search.jsx
@@ -3,14 +3,15 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "f6ac256c253b4a19a3242c3e9fc32a3d";
 
-export default function Search() {
+export default function Search({ foodData, setfoodData }) {
   const [query, setQuery] = useState("pizza");
   //syntax of UseEffect Hook
   useEffect(() => {
     async function fetchFood() {
-      const res = await fetch("${URL}?query=${query}&apiKey=${API_Key}");
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data);
+      console.log(data.results);
+      setfoodData(data.results);
     }
     fetchFood();
   }, [query]);
