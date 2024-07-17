b00ef5f46ee7fd5eb3403df0884596689e747d3a
sumima shakya <sumushakya25@gmail.com>
2024-07-17 10:08:28 +0545

    Form validation

--- reactproj/signup-form/src/FormValidation.jsx
+++ reactproj/signup-form/src/FormValidation.jsx
@@ -8,6 +8,7 @@ export default function FormValidation() {
     password: "",
     confirmPassword: "",
   });
+  const [errors, setErrors] = useState({});
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -19,56 +20,78 @@ export default function FormValidation() {
 
   function handleSubmit(e) {
     e.preventDefault();
+    const validationErrors = {};
+
+    if (!formData.username.trim()) {
+      validationErrors.username = "Username is required!";
+    }
+    if (!formData.email.trim()) {
+      validationErrors.email = "Email is required!";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      validationErrors.email = "This is not a valid email format!";
+    }
+    if (!formData.password.trim()) {
+      validationErrors.password = "Password is required!";
+    } else if (formData.password.length < 4) {
+      validationErrors.password = "Password must be more than 4 characters";
+    } else if (formData.password.length > 8) {
+      validationErrors.password =
+        "Password cannot exceed more than 8 characters";
+    }
+    if (formData.confirmPassword !== formData.password) {
+      validationErrors.confirmPassword = "Password not matched!";
+    }
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      alert("Form submitted Succesfully");
+    }
   }
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter name"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </label>
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        {errors.username && <span>{errors.username}</span>}
       </div>
       <div>
-        <label>
-          Email:
-          <input
-            type="text"
-            name="email"
-            value={formData.email}
-            placeholder="Enter useremail"
-            onChange={handleChange}
-          />
-        </label>
+        <label>Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={formData.email}
+          placeholder="example@gmail.com"
+          onChange={handleChange}
+        />
+        {errors.email && <span>{errors.email}</span>}
       </div>
       <div>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            placeholder="Enter password"
-            onChange={handleChange}
-          />
-        </label>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        {errors.password && <span>{errors.password}</span>}
       </div>
       <div>
-        <label>
-          Confirm Password:
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            placeholder="Confirm your password"
-            onChange={handleChange}
-          />
-        </label>
+        <label>Confirm Password:</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          placeholder="Confirm password"
+          onChange={handleChange}
+        />
+        {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
       </div>
       <div>
         <button type="submit">Submit</button>
