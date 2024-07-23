e20fecd8a0b6fe9644d90d43a363a941d225130b
sumima shakya <sumushakya25@gmail.com>
2024-07-23 10:42:16 +0545

    news-app

---
+++ reactproj/news-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Newsboard from "./components/Newsboard";
+// import Newsitem from "./components/Newsitem";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Newsboard />
+      {/* <Newsitem /> */}
+    </div>
+  );
+};
+
+export default App;
